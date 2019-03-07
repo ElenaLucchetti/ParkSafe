6e7e6ec3a1f6f84582ff9a06a70f7426f5ee001e
Alessia Masola <alessiamasola23@gmail.com>
2019-03-07 14:54:38 +0000

    Changed name of the app, moved My Location button leaving space for Lock button

--- ParkSafe/app/src/main/java/com/example/parksafe/MapsActivity.java
+++ ParkSafe/app/src/main/java/com/example/parksafe/MapsActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -27,6 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private final static int MY_PERMISSION_FINE_LOCATION = 101;
     private FusedLocationProviderClient fusedLocationClient;
+    private SupportMapFragment mapFragment;
 
 
 
@@ -35,8 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -69,6 +71,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             // Enable my current location
             mMap.setMyLocationEnabled(true);
+            // Place My Location button on the upper right corner below the lock button
+            mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+            View locationButton = ((View) mapFragment.getView().findViewById(Integer.parseInt("1")).
+                    getParent()).findViewById(Integer.parseInt("2"));
+            RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+            // position on right bottom
+            rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+            rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
+            rlp.setMargins(0, 200, 30, 0);
             // Get last location (it's almost equivalent to getting the current location of the device) and do something
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
